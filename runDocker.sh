775652e1d6762604b5520be6951d9747c2b324f7
Scott Aaron <dr_saaron@yahoo.com>
2020-09-25 14:53:01 -0500

    switch to using local server for prod

--- runDocker.sh
+++ runDocker.sh
@@ -1,3 +1,8 @@
 #! /bin/ksh
 
-docker run -it -p 3000:3000 -v ~/.blazartech:/root/.blazartech drsaaron/qotdservices
+# get the IP address of the host
+ip=$(ifconfig wlo1 | grep inet | awk '$1=="inet" {print $2}')
+#ip=$(nslookup blazartech-test.csl2otan97lp.us-east-2.rds.amazonaws.com | grep Address| tail -1 | awk '{ print $2 }')
+
+# run the service
+docker run --add-host quoteDBServer:$ip -it -p 3000:3000 -v ~/.blazartech:/root/.blazartech drsaaron/qotdservices
