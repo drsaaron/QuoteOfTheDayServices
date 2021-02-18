6d649cae667bbf976dea2d379a5c5bbf486b51e9
Scott Aaron <dr_saaron@yahoo.com>
2021-02-18 17:16:18 -0600

    update versions

--- runDocker.sh
+++ runDocker.sh
@@ -5,4 +5,7 @@ ip=$(ifconfig wlo1 | grep inet | awk '$1=="inet" {print $2}')
 #ip=$(nslookup blazartech-test.csl2otan97lp.us-east-2.rds.amazonaws.com | grep Address| tail -1 | awk '{ print $2 }')
 
 # run the service
-docker run --add-host quoteDBServer:$ip -it -p 3000:3000 -v ~/.blazartech:/root/.blazartech drsaaron/qotdservices
+containerName=quoteofthedayservices
+docker stop $containerName
+docker rm $containerName
+docker run --name $containerName --add-host quoteDBServer:$ip -d -p 3000:3000 -v ~/.blazartech:/root/.blazartech drsaaron/qotdservices
