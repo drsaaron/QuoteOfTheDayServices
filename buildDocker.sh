b8f90ed2096086f0586a864dd4c48bd937b5f058
Scott Aaron <dr_saaron@yahoo.com>
2021-04-30 16:57:59 -0500

    version update

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-version=$(getPomAttibute.sh version | sed -e 's/-SNAPSHOT//')
+version=$(getPomAttribute.sh version | sed -e 's/-SNAPSHOT//')
 
 docker build -t drsaaron/qotdservices .
 docker tag drsaaron/qotdservices drsaaron/qotdservices:$version
