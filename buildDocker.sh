9ea092d82f0cfded13ae79f23e10c529c9167de8
Scott Aaron <dr_saaron@yahoo.com>
2021-07-28 15:54:02 -0500

    remove common scripts

--- buildDocker.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-version=$(getPomAttribute.sh version | sed -e 's/-[A-Z]*$//')
-imageName=$(dockerImageName.sh)
-
-docker build -t drsaaron/$imageName .
-docker tag drsaaron/$imageName drsaaron/$imageName:$version
--- pushDocker.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-version=$(getPomAttribute.sh version | sed -e 's/-[A-Z]*$//')
-imageName=$(dockerImageName.sh)
-
-docker push drsaaron/$imageName:$version
