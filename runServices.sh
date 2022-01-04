a9f17ec5e243c49faa3d466b04230b5c9823998d
Scott Aaron <dr_saaron@yahoo.com>
2022-01-04 14:36:00 -0600

    version update

--- runServices.sh
+++ runServices.sh
@@ -4,5 +4,5 @@ appEnv=${ENVIRONMENT:-test}
 
 artifact=$(getPomAttribute.sh artifactId)
 version=$(getPomAttribute.sh version)
-version=1.31-RELEASE
+
 java -jar target/$artifact-$version.jar --spring.config.name=application,$appEnv
