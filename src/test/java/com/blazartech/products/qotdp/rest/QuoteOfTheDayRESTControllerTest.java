7bf2843fc723a3c063fa23688edbd18ce1b5b46f
Scott Aaron <dr_saaron@yahoo.com>
2025-01-16 11:21:14 -0600

    resolve mockito deprecation

--- src/test/java/com/blazartech/products/qotdp/rest/QuoteOfTheDayRESTControllerTest.java
+++ src/test/java/com/blazartech/products/qotdp/rest/QuoteOfTheDayRESTControllerTest.java
@@ -51,7 +51,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
